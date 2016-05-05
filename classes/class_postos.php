06063db07111f42077c61481493270e2adcb18a1
babirondo <babirondo@gmail.com>
2016-05-05 20:07:12 -0300

    ja quebrando em diferentes entidades listando dados da entidade maior na grid diversas alteracoes no modelo

--- classes/class_postos.php
+++ classes/class_postos.php
@@ -12,7 +12,7 @@ class Postos{
 	
 	function getPostos($app, $idworkflow ){
 	 
-		$this->con->executa( "Select * from workflow_postos WHere id_workflow = $idworkflow  ");
+		$this->con->executa( "Select * from workflow_postos WHere id_workflow = $idworkflow and principal = 1 ");
 		//$this->con->navega();
 		
 		$i=0;
@@ -57,22 +57,83 @@ class Postos{
 
 	function getLista($app,   $idposto ){
 	
-		$this->con->executa( "select pc.campo, w.valor, p.id processo
-								from processos p
-									inner join workflow_dados w ON (w.idprocesso = p.id)
-									inner join postos_campo pc ON (pc.id = w.idpostocampo)
-								where w.idpostocampo IN ( SELECT idpostocampo FROM  postos_campo_lista WHERE idposto = $idposto  ) ");
+		
+		// BUSCANDO DADOS DO PAI
+		$this->con->executa( "
+				SELECT pc.campo, w.valor, w.idprocesso, p.idpai
+				FROM postos_campo_lista pcl
+				INNER JOIN postos_campo pc ON (pc.id = pcl.idpostocampo)
+				INNER JOIN workflow_postos wp ON (wp.id = pcl.idposto)
+				INNER JOIN workflow_dados w ON (w.idpostocampo = pcl.idpostocampo )
+				INNER JOIN processos p ON (p.id = w.idprocesso)
+		
+				WHERE pcl.idposto =   $idposto and p.idpai is null ");
+		//$this->con->navega();
+		
+		$i=0;
+		while ($this->con->navega(0)){
+			$pai[$this->con->dados["idprocesso"]][$this->con->dados["campo"] ]   = $this->con->dados["valor"];
+		
+			$array["TITULO"][$this->con->dados["campo"]]   = $this->con->dados["campo"];
+		
+			
+			//echo " \n ".$this->con->dados["idprocesso"]." = ".$this->con->dados["campo"]."  = ". $this->con->dados["valor"];			
+			$i++;
+		}
+				
+		 
+		
+		 
+		// BUSCANDO DADOS DO FILHO
+		$this->con->executa( "
+				SELECT pc.campo, w.valor, w.idprocesso, p.idpai
+				FROM postos_campo_lista pcl 
+						INNER JOIN postos_campo pc ON (pc.id = pcl.idpostocampo)
+						INNER JOIN workflow_postos wp ON (wp.id = pcl.idposto)
+						INNER JOIN processos p ON (p.idtipoprocesso =  wp.idtipoprocesso)
+						INNER JOIN workflow_dados w ON (w.idpostocampo = pcl.idpostocampo and p.id = w.idprocesso)
+						
+				WHERE pcl.idposto =$idposto   ");
 		//$this->con->navega();
 	
 		$i=0;
 		while ($this->con->navega(0)){
-			$array["FETCH"][$this->con->dados["processo"]][$this->con->dados["campo"] ]   = $this->con->dados["valor"];
+			
 
+			//$pai[$this->con->dados["idprocesso"]][$this->con->dados["campo"] ]   = $this->con->dados["valor"];
+			
+
+
+			$array["FETCH"][$this->con->dados["idprocesso"]][$this->con->dados["campo"] ]   = $this->con->dados["valor"];
+			if (is_array($pai[$this->con->dados["idpai"]]) )
+			{
+				$array["FETCH"][$this->con->dados["idprocesso"]] = array_merge($array["FETCH"][$this->con->dados["idprocesso"]] ,$pai[$this->con->dados["idpai"]]);
+			}
+				
 			$array["TITULO"][$this->con->dados["campo"]]   = $this->con->dados["campo"];
 				
 			$i++;
 		}
-	
+		 
+	//	var_dump($array["FETCH"]);
+		
+		$this->con->executa( "select * 
+								from posto_acao pa
+									inner join workflow_postos wp ON (wp.id = pa.goto)
+								where pa.idposto =  $idposto    ");
+		//$this->con->navega();
+		
+		$i=0;
+		while ($this->con->navega(0)){
+			$array["ACOES"]  [$this->con->dados["id"] ][acao]   = $this->con->dados["acao"];
+			$array["ACOES"]  [$this->con->dados["id"] ][ir]   = $this->con->dados["goto"];
+			$array["ACOES"]  [$this->con->dados["id"] ][lista]   = $this->con->dados["lista"]; 
+			$array["ACOES"]  [$this->con->dados["id"] ][idworkflow]   = $this->con->dados["id_workflow"];
+				
+			$i++;
+		}
+		 
+		
 		$array["resultado"] = "SUCESSO";
 	
 		$data =  	$array;
--- classes/class_workflow.php
+++ classes/class_workflow.php
@@ -46,18 +46,67 @@ class Workflow{
 			return false;
 		}	
 		$erro = 0;
- 		
-		if (  $this->con->executa( "INSERT INTO  processos (numprocesso, idpai)
-									VALUES ('zzz',null)
-									RETURNING id ", 1 ) === false )
-			$erro = 1;
-		else{
-			$idprocesso =  $this->con->dados["id"];		
+		
+	//	echo "\n $idposto - ".$json[processo][valor];
+		$id_pai = "null";
+		 if ($idposto && $json[processo][valor] ) {
+		 	$sql = "select tp.id
+					from workflow_postos wp
+					left  join tipos_processo tp ON (tp.id = wp.idtipoprocesso)
+					where wp.id  = $idposto";
+		 //	echo "\n $sql";
+		 	
+			$this->con->executa($sql );
+			$this->con->navega(0);
+			$idtipoprocess_posto = 	$this->con->dados["id"];
+		
+		 	$sql = "select idtipoprocesso from processos where id = ".$json[processo][valor];
+		 //	echo "\n $sql";
+			$this->con->executa( $sql);
+			$this->con->navega(0);
+			$idtipoprocess_processo = 	$this->con->dados["idtipoprocesso"];
+		
+			//echo "\n $idtipoprocess_posto != $idtipoprocess_processo";
+			
+			if ( $idtipoprocess_posto != $idtipoprocess_processo){
+				//$idprocesso =  $idtipoprocess_posto;//$json[processo][valor];
+				
+				$id_pai = $json[processo][valor];
+				$json[processo][valor] = null;
+			}
+		 
+				
 		}
-
+		
+		
+		
+		if ( !$json[processo][valor]    )  {
+ 			 
+ 			$this->con->executa( "select tp.id idtipoprocesso
+									from workflow_postos wp
+										inner join tipos_processo tp ON (tp.id = wp.idtipoprocesso)
+									where wp.id = $idposto " );
+ 			$this->con->navega(0);
+ 			
+ 			$sql = "INSERT INTO  processos (idtipoprocesso, idpai, inicio)
+										VALUES ( ".$this->con->dados["idtipoprocesso"]."   ,$id_pai, NOW() )
+										RETURNING id ";
+ 			
+ 		//	echo "\n $sql";
+			if (  $this->con->executa( $sql , 1 ) === false )
+				$erro = 1;
+			else{
+				$idprocesso =  $this->con->dados["id"];		
+			}
+ 		}
+ 				
+		
+				
  	    foreach ($json as $campo => $valor){
- 	    	if (!$this->con->executa( "INSERT INTO workflow_dados (idpostocampo, valor, idprocesso) 
- 	    						  VALUES ('$campo','$valor[valor]', $idprocesso) "  ))
+ 	    	if ($campo == "processo") continue;
+ 	    	
+ 	    	if (!$this->con->executa( "INSERT INTO workflow_dados (idpostocampo, valor, idprocesso, registro) 
+ 	    						  VALUES ('$campo','$valor[valor]', $idprocesso, NOW()) "  ))
  	    		$erro++;
  	    }
 		
